3cc62b22cf02fe15e8db500ac431eca5b9950dee
Blake Fischer <bfischer@MacBook-Pro.local>
2016-12-29 12:58:27 -0800

    Added Game, Lose, Win, and Start scene. Added MusicPlayer asset and LevelManager. Added some support for menu navigation

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer Instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if(Instance != null) {
+			Destroy(gameObject);
+		}
+		else {
+			Instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
